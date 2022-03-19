4387255ac12433196b940ecddaa7c6792199e735
lucy <kkadangj@gmail.com>
2022-03-19 15:17:04 +0900

    Initial Commit

---
+++ c-study/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c-study
+//
+//  Created by 조현진 on 2022/03/19.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
